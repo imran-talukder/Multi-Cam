74271ace3a07c5531e14a7674df3e6e48505e574
Imran Talukder <71202290+imran-talukder@users.noreply.github.com>
2021-01-20 18:13:06 +0600

    Two camera added but not the third number

--- Multi_Cam_App/ViewController.swift
+++ Multi_Cam_App/ViewController.swift
@@ -6,42 +6,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     
     //MARK: -  Properties
-
+    
+    let camManager = CamManager()
+    let recordManager = RecordManager()
     
     let frontViewLayer: ViewForCamera = {
         let view = ViewForCamera()
         view.backgroundColor = .clear
         return view
     }()
+    var frontLayer: AVCaptureVideoPreviewLayer?
     
     let backViewLayer1: ViewForCamera = {
         let view = ViewForCamera()
         view.backgroundColor = .clear
         return view
     }()
+    var backLayer1: AVCaptureVideoPreviewLayer?
     
     let backViewLayer2: ViewForCamera = {
         let view = ViewForCamera()
         view.backgroundColor = .clear
         return view
     }()
+    var backLayer2: AVCaptureVideoPreviewLayer?
     
     let backViewLayer3: ViewForCamera = {
         let view = ViewForCamera()
         view.backgroundColor = .clear
         return view
     }()
+    var backLayer3: AVCaptureVideoPreviewLayer?
+    
+    //MARK: -  viewDidload
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addCameraViews()
         addActionButtons()
+        setup()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        #if targetEnvironment(simulator)
+          let alertController = UIAlertController(title: "Multi Cam-App", message: "Please run on physical device", preferredStyle: .alert)
+          alertController.addAction(UIAlertAction(title: "OK",style: .cancel, handler: nil))
+          self.present(alertController, animated: true, completion: nil)
+          return
+        #endif
+    }
+    
     let startEndtButton: UIButton = UIButton()
     private func addCameraViews() {
         if !view.subviews.contains(frontViewLayer) {
@@ -101,8 +122,107 @@ class ViewController: UIViewController {
         }
     }
     @objc func recordButtonTrigered(_ sender: UIButton) {
-        
+        if sender.titleLabel?.text == "Start" {
+            recordManager.handleSingleTap()
+            sender.setTitle("End Recording", for: .normal)
+        }else {
+            recordManager.handleDoubleTap()
+            sender.setTitle("Start", for: .normal)
+        }
     }
+}
 
+extension ViewController {
+    func setup() {
+        
+        #if targetEnvironment(simulator)
+            return
+        #endif
+        
+        frontLayer = frontViewLayer.videoPreviewLayer
+        backLayer1 = backViewLayer1.videoPreviewLayer
+        backLayer2 = backViewLayer2.videoPreviewLayer
+        backLayer3 = backViewLayer3.videoPreviewLayer
+        dualVideoPermisson()
+    }
+    
+    func dualVideoPermisson(){
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                // The user has previously granted access to the camera.
+                configureDualVideo()
+                break
+                
+            case .notDetermined:
+                
+                AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+                    if granted{
+                        self.configureDualVideo()
+                    }
+                })
+                
+                break
+                
+            default:
+                // The user has previously denied access.
+            DispatchQueue.main.async {
+                let changePrivacySetting = "Device doesn't have permission to use the camera, please change privacy settings"
+                let message = NSLocalizedString(changePrivacySetting, comment: "Alert message when the user has denied access to the camera")
+                let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                
+                alertController.addAction(UIAlertAction(title: "Settings", style: .`default`,handler: { _ in
+                    if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(settingsURL,  options: [:], completionHandler: nil)
+                    }
+                }))
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func configureDualVideo() {
+        camManager.configureDualVideo(viewController: self) { [weak self] in
+            guard let self = self else { return }
+            
+            guard self.camManager.setUpCamera(type: .builtInWideAngleCamera, position: .front, outputViewlayer: self.frontLayer!) else{
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Error", message: "issue while setuping front camera", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK",style: .cancel, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                return
+            }
+            
+//            guard self.camManager.setUpCamera(type: .builtInWideAngleCamera, position: .back, outputViewlayer: self.backLayer1!) else{
+//                DispatchQueue.main.async {
+//                    let alertController = UIAlertController(title: "Error", message: "issue while setuping back camera", preferredStyle: .alert)
+//                    alertController.addAction(UIAlertAction(title: "OK",style: .cancel, handler: nil))
+//                    self.present(alertController, animated: true, completion: nil)
+//                }
+//                return
+//            }
+            
+            guard self.camManager.setUpCamera(type: .builtInTelephotoCamera, position: .back, outputViewlayer: self.backLayer2!) else{
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Error", message: "3rd camera", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK",style: .cancel, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                return
+            }
+            
+            guard self.camManager.setUpCamera(type: .builtInTrueDepthCamera, position: .back, outputViewlayer: self.backLayer3!) else{
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Error", message: "4th camera", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK",style: .cancel, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                return
+            }
+        }
+    }
 }
 
