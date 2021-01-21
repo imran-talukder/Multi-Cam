909b1dc6ecbdbe47207b30c0f450d8c307c9d664
Imran Talukder <71202290+imran-talukder@users.noreply.github.com>
2021-01-21 10:15:16 +0600

    Video saved to gallery

--- Multi_Cam_App/ViewController.swift
+++ Multi_Cam_App/ViewController.swift
@@ -67,43 +67,44 @@ class ViewController: UIViewController {
     private func addCameraViews() {
         if !view.subviews.contains(frontViewLayer) {
             view.addSubview(frontViewLayer)
+            frontViewLayer.backgroundColor = .gray
+            frontViewLayer.translatesAutoresizingMaskIntoConstraints = false
+            frontViewLayer.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+            frontViewLayer.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+            frontViewLayer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
+            frontViewLayer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         }
-        frontViewLayer.backgroundColor = .gray
-        frontViewLayer.translatesAutoresizingMaskIntoConstraints = false
-        frontViewLayer.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
-        frontViewLayer.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        frontViewLayer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
-        frontViewLayer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         
         if !view.subviews.contains(backViewLayer1) {
             view.addSubview(backViewLayer1)
+            backViewLayer1.backgroundColor = .gray
+            backViewLayer1.translatesAutoresizingMaskIntoConstraints = false
+            backViewLayer1.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+            backViewLayer1.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+            backViewLayer1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
+            backViewLayer1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         }
-        backViewLayer1.backgroundColor = .gray
-        backViewLayer1.translatesAutoresizingMaskIntoConstraints = false
-        backViewLayer1.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
-        backViewLayer1.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        backViewLayer1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
-        backViewLayer1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         
         if !view.subviews.contains(backViewLayer2) {
             view.addSubview(backViewLayer2)
+            backViewLayer2.backgroundColor = .gray
+            backViewLayer2.translatesAutoresizingMaskIntoConstraints = false
+            backViewLayer2.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+            backViewLayer2.topAnchor.constraint(equalTo: frontViewLayer.bottomAnchor, constant: 10).isActive = true
+            backViewLayer2.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
+            backViewLayer2.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         }
-        backViewLayer2.backgroundColor = .gray
-        backViewLayer2.translatesAutoresizingMaskIntoConstraints = false
-        backViewLayer2.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
-        backViewLayer2.topAnchor.constraint(equalTo: frontViewLayer.bottomAnchor, constant: 10).isActive = true
-        backViewLayer2.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
-        backViewLayer2.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         
         if !view.subviews.contains(backViewLayer3) {
             view.addSubview(backViewLayer3)
+            backViewLayer3.backgroundColor = .gray
+            backViewLayer3.translatesAutoresizingMaskIntoConstraints = false
+            backViewLayer3.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+            backViewLayer3.topAnchor.constraint(equalTo: backViewLayer1.bottomAnchor, constant: 10).isActive = true
+            backViewLayer3.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
+            backViewLayer3.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
         }
-        backViewLayer3.backgroundColor = .gray
-        backViewLayer3.translatesAutoresizingMaskIntoConstraints = false
-        backViewLayer3.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
-        backViewLayer3.topAnchor.constraint(equalTo: backViewLayer1.bottomAnchor, constant: 10).isActive = true
-        backViewLayer3.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35).isActive = true
-        backViewLayer3.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.45).isActive = true
+        
     }
     
     private func addActionButtons() {
@@ -127,6 +128,9 @@ class ViewController: UIViewController {
             sender.setTitle("End Recording", for: .normal)
         }else {
             recordManager.handleDoubleTap()
+            let alertController = UIAlertController(title: "Saved!", message: "Video saved to your gallery.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Done",style: .cancel, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
             sender.setTitle("Start", for: .normal)
         }
     }
@@ -144,6 +148,7 @@ extension ViewController {
         backLayer2 = backViewLayer2.videoPreviewLayer
         backLayer3 = backViewLayer3.videoPreviewLayer
         dualVideoPermisson()
+        recordManager.configure(viewController: self)
     }
     
     func dualVideoPermisson(){
